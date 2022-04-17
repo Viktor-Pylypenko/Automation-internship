c9c10a95f9259f3d542db5272e32600d26f010ae
Viktor Pylypenko <pylypenko.viktor@airslate.com>
2022-04-17 23:09:08 +0300

    debug

--- src/test/java/junk/NewFile.java
+++ src/test/java/junk/NewFile.java
@@ -4,5 +4,6 @@ public class NewFile {
     public static void main(String[] args) {
         System.out.printf("new file");
         System.out.printf("debug");
+        System.out.printf("debug1");
     }
 }
