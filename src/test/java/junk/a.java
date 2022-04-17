c09c928523e610bbd3c08e105f9c2328466ef854
Viktor Pylypenko <34416976+Viktor-Pylypenko@users.noreply.github.com>
2022-04-17 23:02:25 +0300

    new file (#9)
    *squashed
    Co-authored-by: Viktor Pylypenko <pylypenko.viktor@airslate.com>

---
+++ src/test/java/junk/a.java
@@ -0,0 +1,12 @@
+package junk;
+
+public class a {
+    public static void main(String[] args) {
+        System.out.printf("a");
+        System.out.printf("b");
+        System.out.printf("c");
+        System.out.printf("d");
+        System.out.printf("e");
+        System.out.printf("neew");
+    }
+}
