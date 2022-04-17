ced8a8aac86a3c13f3b803d0fdedf5ebb86485cb
Viktor Pylypenko <pylypenko.viktor@airslate.com>
2022-04-17 22:49:58 +0300

    debug

--- src/test/java/junk/a.java
+++ src/test/java/junk/a.java
@@ -4,5 +4,8 @@ public class a {
     public static void main(String[] args) {
         System.out.printf("a");
         System.out.printf("b");
+        System.out.printf("c");
+        System.out.printf("d");
+        System.out.printf("e");
     }
 }
