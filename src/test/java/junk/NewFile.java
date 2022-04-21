511b739589daae7c633dad32c3c686bee05fc059
Viktor Pylypenko <pylypenko.viktor@airslate.com>
2022-04-21 23:52:54 +0300

    Revert "Wrong commit"
    This reverts commit bb6dc07b30334bd189d0befbe95305440af41ac0.

--- src/test/java/junk/NewFile.java
+++ src/test/java/junk/NewFile.java
@@ -3,6 +3,5 @@ package junk;
 public class NewFile {
     public static void main(String[] args) {
         System.out.printf("new file");
-        System.out.printf("WRONG COMMIT");
     }
 }
--- src/test/java/junk/PVNew.java
+++ src/test/java/junk/PVNew.java
@@ -3,6 +3,5 @@ package junk;
 public class PVNew {
     public static void main(String[] args) {
         System.out.println("first commit");
-        System.out.printf("WRONG COMMIT");
     }
 }
