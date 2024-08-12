48ccb8e9a0d80b4a7266c55f34c07fad66e6ed92
Viktor <vitya.slot@gmail.com>
2024-08-12 21:01:26 +0200

    Add feature 3

--- src/test/java/exmple/Example.java
+++
@@ -1,8 +0,0 @@
-package exmple;
-
-public class Example {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World Two!");
-    }
-}
