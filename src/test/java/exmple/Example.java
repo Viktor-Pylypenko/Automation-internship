8c270ec47c523daff2fd0dce335f0db2c76e8373
Viktor <vitya.slot@gmail.com>
2024-08-12 20:28:40 +0200

    Add second line

--- src/test/java/exmple/Example.java
+++ src/test/java/exmple/Example.java
@@ -3,5 +3,6 @@ package exmple;
 public class Example {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("Hello, World Two!");
     }
 }
